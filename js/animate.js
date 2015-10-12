92d4129535e0618e256026dd1a7783272ded31ac
Anand Upadhyay <anandu@gumromedia.com>
2015-10-12 09:17:16 -0400

    updated animation

--- js/animate.js
+++ js/animate.js
@@ -2,35 +2,36 @@ function animationHover(element, animationup, animationdown){
   element = $(element);
   element.hover(
     function() {
-      element.removeClass('animated ' + animationdown);    
-      element.addClass('animated ' + animationup);    
+      element.removeClass('animated ' + animationdown);
+      element.addClass('animated ' + animationup);
     },
     function(){
       //wait for animation to finish before removing classes
       window.setTimeout( function(){
-        element.removeClass('animated ' + animationup);  
+        element.removeClass('animated ' + animationup);
         element.addClass('animated ' + animationdown);
       }, 100);
     }
   );
 };
-    
+
 animationHover('#card1', 'pulseup', 'pulsedown');
 animationHover('#card2', 'pulseup', 'pulsedown');
-animationHover('#card3', 'pulseup', 'pulsedown'); 
-animationHover('#card4', 'pulseup', 'pulsedown'); 
+animationHover('#card3', 'pulseup', 'pulsedown');
+animationHover('#card4', 'pulseup', 'pulsedown');
+animationHover('#card5', 'pulseup', 'pulsedown');
 
 
 function animationHover2(element, animation){
   element = $(element);
   element.hover(
-    function() {  
-      element.addClass('animated ' + animation);    
+    function() {
+      element.addClass('animated ' + animation);
     },
     function(){
       //wait for animation to finish before removing classes
       window.setTimeout( function(){
-        element.removeClass('animated ' + animation);  
+        element.removeClass('animated ' + animation);
       }, 500);
     }
   );
